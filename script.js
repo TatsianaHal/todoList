9bca23b588e815b4e1663b928b22722dc1311af9
TatsianaHal <tripwithtanya@gmail.com>
2018-05-07 11:42:33 +0300

    07/05/2018

--- script.js
+++ script.js
@@ -1,55 +1,188 @@
-let list = document.getElementById('list'); 
-const addBtn = document.querySelector('.add'); 
+const addBtn = document.querySelector('.add');
 
-addBtn.onclick = () => { 
-    const li = document.createElement('li');
-    const inputFirst = document.querySelector('.in').value;
-    const inputSecond = document.querySelector('.date').value;
+let list = document.getElementById('list');
+let li = document.createElement('li');
 
-    const inputInfo = inputFirst + ' ' + inputSecond;
-    li.innerHTML = inputInfo;
+let todos = []; 
+
+let getTodos = () => {
+    if(localStorage.getItem('todos')) {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+}
+
+let push = () => {
+    let li = document.createElement('li');
+    let inputFirst = document.querySelector('.in').value;
+    let inputSecond = document.querySelector('.date').value;
+    let inputInfo = inputFirst + ' ' + inputSecond;
+    let textN = document.createTextNode(inputInfo);
+
+    const dellBtn = document.createElement('span');
+    const dellTxt = document.createTextNode(' X');
+
+    dellBtn.className = 'trash';
+
+    li.appendChild(textN);
+    dellBtn.appendChild(dellTxt);
+    li.appendChild(dellBtn);
     list.appendChild(li);
-
-
-    // let arr = localStorage.arr ? JSON.parse(localStorage.getItem('arr')) : [];
-
-    // arr.push({
-    //     text: inputFirst,
-    //     data: inputSecond
-    // });
-
-    // localStorage.setItem('arr', JSON.stringify(arr));
-    // console.log(arr);
-
+    
 
 
     li.addEventListener('click', () => { 
         li.classList.toggle('checked'); 
     });
 
+    dellBtn.addEventListener('click', () => {
+        const div = dellBtn.parentNode;
+        div.remove();
+    });
+
+
+
     if(!inputFirst || !inputSecond) {
         alert('Введите необходимые данные');
-        document.getElementById('list').removeChild(li); 
+        list.removeChild(li); 
     } else {
-        document.getElementById('list').appendChild(li);
+        list.appendChild(li);
     }
+
+
 
     document.querySelector('.in').value = '';
     document.querySelector('.date').value = '';
 
-    const dellBtn = document.createElement('button');
-    dellBtn.innerHTML = 'Dell';
-    dellBtn.className = 'trash';
-    li.appendChild(dellBtn);    
 
-    dellBtn.addEventListener('click', () => {
-        const div = dellBtn.parentNode;
-        div.remove();
-    }); 
+    let todo = {};
+    todo.text = inputInfo;
+    todo.btn = dellBtn;
+    let i = todos.length;
+    todos[i] = todo;
+    console.log(todos);
+
+    localStorage.setItem('todos', JSON.stringify(todos));   
+    
+    console.log(localStorage);
 }
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputFirst = document.querySelector('.in');
+// const inputSecond = document.querySelector('.date');
+// const addBtn = document.querySelector('.add');
+// let list = document.getElementById('list');
+
+// let li = document.createElement('li');
+
+// // let todos = []; 
+
+// // let getTodos = () => {
+// //     if(localStorage.getItem('todos')) {
+// //         todos = JSON.parse(localStorage.getItem('todos'));
+// //     }
+// // }
+
+// let push = () => {
+//     //let li = document.createElement('li');
+//     const text = inputFirst.value;
+//     const date = inputSecond.value;
+
+//     console.log(text);
+//     console.log(date);
+//     // let inputInfo = inputFirst + ' ' + inputSecond;
+//     // let textN = document.createTextNode(inputInfo);
+
+//     if(inputFirst && inputSecond) {
+//         localStorage.setItem(inputFirstVal, inputSecondVal);
+//         location.reload();
+//     // } else {
+//     //     list.appendChild(li);
+//     }
+
+//     for(let i = 0; i < localStorage.length; i++) {
+//         const inputFirstVal = localStorage.key(i);
+//         const inputSecondVal = localStorage.getItem(inputFirstVal);
+
+//         li.innerHTML += 
+
+//     }
+
+//     const dellBtn = document.createElement('span');
+//     const dellTxt = document.createTextNode(' X');
+
+//     dellBtn.className = 'trash';
+
+//     li.appendChild(textN);
+//     dellBtn.appendChild(dellTxt);
+//     li.appendChild(dellBtn);
+//     list.appendChild(li);
     
+
+
+//     li.addEventListener('click', () => { 
+//         li.classList.toggle('checked'); 
+//     });
+
+//     dellBtn.addEventListener('click', () => {
+//         const div = dellBtn.parentNode;
+//         div.remove();
+//     });
+
+//     document.querySelector('.in').value = '';
+//     document.querySelector('.date').value = '';
+
+
+//     let todo = {};
+//     todo.text = inputInfo;
+//     todo.btn = dellBtn;
+//     let i = todos.length;
+//     todos[i] = todo;
+//     console.log(todos);
+
+//     localStorage.setItem('todos', JSON.stringify(todos));   
     
+//     console.log(localStorage);
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
